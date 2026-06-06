9d2ee06493f7af75f900c109951c770dab2b1ec4
davidar3-art <davidar3@ac.sce.ac.il>
2026-06-06 12:49:27 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -1,8 +1,11 @@
-data = np.array(input_array)
+def normalized_array(input_array):
+  data = np.array(input_array)
   if np.all(data == data[0]):
-   return np.zeros(data.shape)
+    return np.zeros(data.shape)
   else:
     new_array = (data - np.min(data)) / (np.max(data) - np.min (data))
+    
+return new_array
     # חשוב לזכור להחליף את pass ב- return
 
 if __name__ == "__main__":
